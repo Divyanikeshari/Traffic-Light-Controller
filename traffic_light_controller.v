2d723500799e52fdb3f0d42debf8942625b06752
Divyani Keshari <divyanik0910@gmail.com>
2024-08-14 01:04:10 +0530

    Update traffic_light_controller.v

--- traffic_light_controller.v
+++ traffic_light_controller.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 
 module signal_controller( hwy, contry, X, clk, clr );
+//declaring input and output ports
 input clk,clr;
 input X;
 output [1:0] hwy,contry; 
@@ -12,13 +13,13 @@ parameter GREEN =2'b10;
 
 reg [2:0] state;
 reg [2:0] next_state;
-
+//state declaration
 parameter S0= 3'b000;
 parameter S1 =3'b001;
 parameter S2 =3'b010;
 parameter S3 =3'b011;
 parameter S4 =3'b100;
-
+//state logic
 always @ (posedge clk or posedge clr) 
 begin
         if (clr)
@@ -29,10 +30,9 @@ end
 always @ (*)
 state=next_state;
 always @(*)
-begin
-
+begi
 case(state)
-S0: begin 
+S0: begin //initial state where no car is sensed in countryside road
         hwy=GREEN;
         contry= RED;
         if(X)
@@ -40,19 +40,20 @@ S0: begin
     else
         next_state=S0;
         end 
-S1: begin 
+S1: begin //car detected in country side road and now traffic signal is changing from green to yellow of highway road
+        #20
         hwy=YELLOW;
         contry= RED;
-        //@(posedge clk);
         next_state=S2;
         end 
-S2: begin 
+S2: begin //traffic signal changes from yellow to red for highway road
+        #20
         hwy=RED;
         contry= RED;
-        //@(posedge clk);
         next_state = S3;
         end 
-S3: begin 
+S3: begin //the traffic signal of countryside road give green signal and highway road give red signal stays in this state untill all cars from countryside road gone
+        #20
         hwy=RED;
         contry= GREEN;
         if(X)
@@ -60,19 +61,13 @@ S3: begin
      else
         next_state= S4;
         end 
-S4: begin 
+S4: begin //now no car is left in country side road so signal is change from green to yellow then red
+        #20
         hwy=RED;
         contry= YELLOW;
-       // @(posedge clk);
         next_state=S0;
         end 
-
-
-
 default: next_state=S0;
 endcase
 end 
-
-                
-
 endmodule
